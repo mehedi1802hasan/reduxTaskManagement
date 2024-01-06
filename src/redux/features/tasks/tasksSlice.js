d42848dbbde988618c419997cd8261e06446103d
mehedi1802hasan <mehedi1802hasan@gmail.com>
2024-01-06 15:41:08 +0600

    first commit

---
+++ src/redux/features/tasks/tasksSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    tasks :[]
+}
+
+const tasksSlice =createSlice({
+name:'tasksSlice',
+initialState,
+reducers:{}
+
+})
+export default tasksSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import tasksSlice from './features/tasks/tasksSlice';
+const store = configureStore({
+    reducer:{
+        tasks:tasksSlice,
+    }
+})
+export default store;
